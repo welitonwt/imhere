b6ae60f1be2ddbeacf505551a9f653cb3a015df5
weliton <weliton.ww@hotmail.com>
2024-01-18 13:17:07 -0300

    create  data input

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -4,13 +4,24 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#131016',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
   eventName: {
     color: '#fff',
     fontSize: 24,
     fontWeight: 'bold',
-    marginTop: 48
+    marginTop: 15
+  },
+  eventData: {
+    color: '#C4C2C2',
+    fontSize: 14,
+  },
+  input:{
+    height: 36,
+    borderRadius: 5,
+    backgroundColor: '#1f1e25',
+    color: '#fff',
+    padding: 6,
+    fontSize: 16
   }
 })
