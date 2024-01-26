6d9dd5ac65adbd603726f74498032dd4f05bf4f6
weliton <weliton.ww@hotmail.com>
2024-01-26 17:14:54 -0300

    create button and form

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -6,22 +6,48 @@ export const styles = StyleSheet.create({
     backgroundColor: '#131016',
 
   },
+
   eventName: {
     color: '#fff',
     fontSize: 24,
     fontWeight: 'bold',
     marginTop: 15
   },
+
   eventData: {
     color: '#C4C2C2',
     fontSize: 14,
   },
+
+  form: {
+    width: '100%',
+    flexDirection: 'row',
+    marginTop: 36,
+    marginBottom: 42,
+ },
+
   input:{
-    height: 36,
+    flex: 1,
+    height: 56,
     borderRadius: 5,
     backgroundColor: '#1f1e25',
     color: '#fff',
     padding: 6,
-    fontSize: 16
+    fontSize: 16,
+    marginRight: 12
+  },
+
+  buttonContainer:{
+    backgroundColor: '#115515',
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  buttonText: {
+    color: '#fff',
+    fontSize: 24
   }
 })
